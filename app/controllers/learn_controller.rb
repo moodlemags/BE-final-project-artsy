7cd700a2be26aaf3be428335129bcf806bb1e7d3
Maggie Merrell <maggiemerrell31@gmail.com>
2016-09-01 19:03:24 -0400

    bleh

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -21,23 +21,30 @@ class LearnController < ApplicationController
       render json: {'Welcome controller': 'welcome to the Artsy API woowooowow'}
     end
 
+
     def trigger_artsy_api
       data_received = params[:artist]
       puts "artist params #{data_received}"
-      xapp_token = 'JvTPWe4WsQO-xqX6Bts49id7qJqr5JLsQGAdvulqJSW9mftRPIKGBuNJnrA0JfQ8-dFZSrZeKVEBsJGHtE6lIT1otchC4Yw6uavJ3kY7STUddLq2MkVxx7KoIKFD3oTy2ltss5IeJT2lhszx6oM0W9YK1nm3aoC8F1Nxo4LPfiYaAhNW_k4W2GrspCa2SZfpZbmxT351ecFpgcfId8mq3iq5sfRkVMy5V8zs4nskkx4='
 
-        api = Hyperclient.new('https://api.artsy.net/api') do |api|
-          api.headers['Accept'] = 'application/vnd.artsy-v2+json'
-          api.headers['X-Xapp-Token'] = xapp_token
-          api.connection(default: false) do |conn|
-            conn.use FaradayMiddleware::FollowRedirects
-            conn.use Faraday::Response::RaiseError
-            conn.request :json
-            conn.response :json, content_type: /\bjson$/
-            conn.adapter :net_http
-          end
+      client_id = '45c1a28f62560c431645'
+      client_secret = '3fb01aaf998e1ef3fea830d232a36c05'
+
+      # Get token
+      xapp_token = HTTParty.post("https://api.artsy.net/api/tokens/xapp_token?client_id=#{client_id}&client_secret=#{client_secret}")
+      xapp_token = xapp_token["token"]
+      puts "token!:", xapp_token
+
+      api = Hyperclient.new('https://api.artsy.net/api') do |api|
+        api.headers['Accept'] = 'application/vnd.artsy-v2+json'
+        api.headers['X-Xapp-Token'] = xapp_token
+        api.connection(default: false) do |conn|
+          conn.use FaradayMiddleware::FollowRedirects
+          conn.use Faraday::Response::RaiseError
+          conn.request :json
+          conn.response :json, content_type: /\bjson$/
+          conn.adapter :net_http
         end
-
+      end
         endpoint = "#{data_received}+more:pagemap:metatags-og_type:artist".to_s
         search_params = "#{endpoint}".to_s
         artist_search = api.search(q: endpoint.to_s)
@@ -85,19 +92,25 @@ class LearnController < ApplicationController
       data_received = params[:artist_id]
       puts "artist id #{data_received}"
 
-      xapp_token = 'JvTPWe4WsQO-xqX6Bts49id7qJqr5JLsQGAdvulqJSW9mftRPIKGBuNJnrA0JfQ8-dFZSrZeKVEBsJGHtE6lIT1otchC4Yw6uavJ3kY7STUddLq2MkVxx7KoIKFD3oTy2ltss5IeJT2lhszx6oM0W9YK1nm3aoC8F1Nxo4LPfiYaAhNW_k4W2GrspCa2SZfpZbmxT351ecFpgcfId8mq3iq5sfRkVMy5V8zs4nskkx4='
+      client_id = '45c1a28f62560c431645'
+      client_secret = '3fb01aaf998e1ef3fea830d232a36c05'
 
-        api = Hyperclient.new('https://api.artsy.net/api') do |api|
-          api.headers['Accept'] = 'application/vnd.artsy-v2+json'
-          api.headers['X-Xapp-Token'] = xapp_token
-          api.connection(default: false) do |conn|
-            conn.use FaradayMiddleware::FollowRedirects
-            conn.use Faraday::Response::RaiseError
-            conn.request :json
-            conn.response :json, content_type: /\bjson$/
-            conn.adapter :net_http
-          end
+      # Get token
+      xapp_token = HTTParty.post("https://api.artsy.net/api/tokens/xapp_token?client_id=#{client_id}&client_secret=#{client_secret}")
+      xapp_token = xapp_token["token"]
+      puts "token!:", xapp_token
+
+      api = Hyperclient.new('https://api.artsy.net/api') do |api|
+        api.headers['Accept'] = 'application/vnd.artsy-v2+json'
+        api.headers['X-Xapp-Token'] = xapp_token
+        api.connection(default: false) do |conn|
+          conn.use FaradayMiddleware::FollowRedirects
+          conn.use Faraday::Response::RaiseError
+          conn.request :json
+          conn.response :json, content_type: /\bjson$/
+          conn.adapter :net_http
         end
+      end
 
         get_gene_id = api.genes(artist_id: data_received, total_count: 1)
         learn_one_gene = get_gene_id._embedded.genes[0].name
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
 
-get   "/"                   =>      "learn#index"
+# ROOT ROUTE
+get "/"           => "game#welcome_placeholder"
+get "/artsy"      => "game#make_artsy_api_call"
+
+
+# GAME ROUTES
+get   "/game"                   =>      "game#get_game_data"
+post  "/game/:gene"            =>      "game#find_gene"
+
+
+# LEARN ROUTES
 post  "/learn"              =>      "learn#trigger_artsy_api"
 get   "/learn"              =>      "learn#index"
 post  "/learn/search"       =>      "learn#render_artsy"
+
 post  "/learn/search/:gene"  =>     "learn#render_artsy"
 post  "/game"               =>      "game#get_game_data"
+#
+#
 
-post   "/game/:gene"         =>     "game#find_gene"
 end
